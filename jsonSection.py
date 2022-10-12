8c3f6dff7e0bc579f1ab7cfd71b458d036436845
Lari <69885449+laralopez17@users.noreply.github.com>
2022-10-12 18:23:26 -0300

    Only missing item search for changing taxes
    The programm already changes all the taxes from items, master Options and set Options, on selected sections and there's an option to change it for all sections too.

--- jsonSection.py
+++ jsonSection.py
@@ -1,30 +1,85 @@
-# This Python file uses the following encoding: utf-8
-
 import json
-#from PySide6.QtWidgets import QListWidgetItem, QListWidget
-#from mainwindow import MainWindow
-#from ui_form import Ui_MainWindow
 
 class jsonSection:
     def __init__(self):
         pass
 
     def openJson(self,selectedFile):
-        if ".json" in selectedFile:
-            with open(selectedFile, encoding="utf-8") as archivoJson:
-                datos = json.load(archivoJson)
-            j = len(datos["MenuSections"])
+        with open(selectedFile, encoding="utf-8") as archivoJson:
+            datos = json.load(archivoJson)
+        return datos
 
-            for i in range (0,j):
-               # print(datos["MenuSections"][i]["Name"])
-                item1 = datos["MenuSections"][i]["Name"]
-                print(item1)
+    #sends the tax rates available to the mainwindow
+    def slot_mostrarTax(self,datos):
+        taxes = list()
+        for tax in datos['TaxRates']:
+            taxes.append(tax['Name']+ " " + str(tax['Rate'])+ "%")
+
+        print(taxes)
+        return taxes
+    #sends the sections available to the mainwindow
+    def slot_mostrarSecciones(self,datos):
+        secciones = list()
+        for section in datos['MenuSections']:
+            secciones.append(section['Name'])
+        return secciones
 
 
-            #item = QListWidgetItem(item1)
-            #widget.ui.sectionlist.addItem(item1)
-            #widget.show()
 
-            #print(item)
-            #widget.ui.sectionlist(widget,item)
-            #widget.sectionItems(MainWindow,item)
+    #changes the taxes for the selected sections (only items for now)
+    def slot_changeTaxSelectedSections(self,datos,secciones,tax):
+        newTaxRateId = 0
+        for i in range (0,len(datos['TaxRates'])):
+            if i == tax:
+                newTaxRateId = datos['TaxRates'][i]['TaxRateId']
+        for j in range(0,len(secciones)):
+            for i in range(0,len(datos["MenuSections"])):
+                section = datos["MenuSections"][i]["Name"]
+                if section == secciones[j]:
+                    for k in range(0,len(datos["MenuSections"][i]["MenuItems"])):
+                        for item in datos["MenuSections"][i]["MenuItems"]:
+                            item['TaxRateId'] = newTaxRateId
+                            for l in range(0,len(datos["MenuSections"][i]["MenuItems"][k]["MenuItemOptionSets"])):
+                                for os in datos["MenuSections"][i]["MenuItems"][k]["MenuItemOptionSets"]:
+                                    for osi in datos["MenuSections"][i]["MenuItems"][k]["MenuItemOptionSets"][l]['MenuItemOptionSetItems']:
+                                        osi['TaxRateId'] = newTaxRateId
+
+
+    #changes the taxes for the selected items (only items for now)
+    def slot_changeTaxSelectedItems(self,datos,itemSelected,tax):
+        newTaxRateId = 0
+        for i in range (0,len(datos['TaxRates'])):
+            if i == tax:
+                newTaxRateId = datos['TaxRates'][i]['TaxRateId']
+                for j in range(0,len(itemSelected)):
+                    for i in range(0,len(datos["MenuSections"])):
+                            for k in range(0,len(datos["MenuSections"][i]["MenuItems"])):
+                                for item in datos["MenuSections"][i]["MenuItems"]:
+                                    items = datos["MenuSections"][i]["MenuItems"][k]["Name"]
+                                    if items == itemSelected[j]:
+                                        item['TaxRateId'] = newTaxRateId
+                                        for l in range(0,len(datos["MenuSections"][i]["MenuItems"][k]["MenuItemOptionSets"])):
+                                            for os in datos["MenuSections"][i]["MenuItems"][k]["MenuItemOptionSets"]:
+                                                for osi in datos["MenuSections"][i]["MenuItems"][k]["MenuItemOptionSets"][l]['MenuItemOptionSetItems']:
+                                                    osi['TaxRateId'] = newTaxRateId
+
+
+    #changes the taxes for all sections (only items for now)
+    def slot_changeTaxAllSections(self,datos,tax):
+        newTaxRateId = 0
+        for i in range (0,len(datos['TaxRates'])):
+            if i == tax:
+                newTaxRateId = datos['TaxRates'][i]['TaxRateId']
+            for i in range (0,len(datos["MenuSections"])):
+                for k in range(0,len(datos["MenuSections"][i]["MenuItems"])):
+                    for item in datos["MenuSections"][i]["MenuItems"]:
+                        item['TaxRateId'] = newTaxRateId
+                        for l in range(0,len(datos["MenuSections"][i]["MenuItems"][k]["MenuItemOptionSets"])):
+                            for os in datos["MenuSections"][i]["MenuItems"][k]["MenuItemOptionSets"]:
+                                for osi in datos["MenuSections"][i]["MenuItems"][k]["MenuItemOptionSets"][l]['MenuItemOptionSetItems']:
+                                    osi['TaxRateId'] = newTaxRateId
+
+    #overwrites the json file with the updated one
+    def save_json(self,datos,selectedFile):
+        with open(selectedFile, 'w') as archivo_nuevo:
+            json.dump(datos, archivo_nuevo)
